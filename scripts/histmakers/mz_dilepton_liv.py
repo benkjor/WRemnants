5c57cd79439d41adebf8c57417fcbc2e64c93e24
benkjor <jorian.benke@gmail.com>
2025-06-30 10:38:05 -0400

    switch to tightID and trigger criterion

--- scripts/histmakers/mz_dilepton_liv.py
+++ scripts/histmakers/mz_dilepton_liv.py
@@ -6,14 +6,10 @@ from datetime import datetime
 import narf
 
 from utilities import common, parsing
-from wremnants import (muon_selections, muon_calibration)
+from wremnants import (muon_selections, muon_calibration, pileup, vertex)
 from wremnants.datasets.datagroups import Datagroups
 from wremnants.datasets.dataset_tools import getDatasets
-from wremnants.histmaker_tools import (
-    aggregate_groups,
-    scale_to_data,
-    write_analysis_output,
-)
+from wremnants.histmaker_tools import (aggregate_groups, scale_to_data,  write_analysis_output)
 from wums import logging
 from narf.lumitools import make_brilcalc_helper
 
@@ -62,11 +58,36 @@ def make_timehelper(filename):
 args = parser.parse_args()
 logger = logging.setup_logger(__file__, args.verbose, args.noColorLogger)
 era = args.era
+calib_filepaths = common.calib_filepaths
 
 #hoping this can go up top
 lumicsv = f"{common.data_dir}/bylsoutput.csv" 
 brilcalc_helper = make_timehelper(lumicsv)
+mc_calibration_helper, data_calibration_helper, calibration_uncertainty_helper = ( muon_calibration.make_muon_calibration_helpers(args, era=era))
 
+(
+    mc_jpsi_crctn_helper,
+    data_jpsi_crctn_helper,
+    mc_jpsi_crctn_unc_helper,
+    data_jpsi_crctn_unc_helper,
+) = muon_calibration.make_jpsi_crctn_helpers(
+    args, calib_filepaths, make_uncertainty_helper=True)
+
+smearing_helper, smearing_uncertainty_helper = (
+    (None, None) if args.noSmearing else muon_calibration.make_muon_smearing_helpers()
+)
+pileup_helper = pileup.make_pileup_helper(era=era)
+vertex_helper = vertex.make_vertex_helper(era=era)
+# muon_prefiring_helper, muon_prefiring_helper_stat, muon_prefiring_helper_syst = (   muon_prefiring.make_muon_prefiring_helpers(era=era))
+    
+    
+# if args.binnedScaleFactors:
+
+#     muon_efficiency_helper, muon_efficiency_helper_syst, muon_efficiency_helper_stat = (
+#         muon_efficiencies_binned.make_muon_efficiency_helpers_binned(
+#             filename=args.sfFile, era=era, max_pt=args.pt[2], is_w_like=True
+        # )
+    # )
 datasets = getDatasets(
     maxFiles=args.maxFiles,
     filt=args.filterProcs,
@@ -80,6 +101,9 @@ datasets = getDatasets(
 axis_date = hist.axis.Regular(24, 0, 24, name = 'time')
 
 
+
+
+########################################################
 def build_graph_lumi(df, dataset):
     df = df.Define("time", brilcalc_helper, ["run", "luminosityBlock"])
     hist_time = df.HistoBoost("time", [axis_date], ['time', 'lumival'])
@@ -89,124 +113,93 @@ def build_graph_lumi(df, dataset):
 def build_graph(df, dataset):
     logger.info(f"build graph for dataset: {dataset.name}")
     era = args.era
-    calib_filepaths = common.calib_filepaths
     results = []
-    
-    mc_calibration_helper, data_calibration_helper, calibration_uncertainty_helper = (
-    muon_calibration.make_muon_calibration_helpers(args, era=era)
-    )
-    (
-        mc_jpsi_crctn_helper,
-        data_jpsi_crctn_helper,
-        mc_jpsi_crctn_unc_helper,
-        data_jpsi_crctn_unc_helper,
-    ) = muon_calibration.make_jpsi_crctn_helpers(
-        args, calib_filepaths, make_uncertainty_helper=True)
-    
-    
-    
-    cvh_helper = data_calibration_helper if dataset.is_data else mc_calibration_helper
-    jpsi_helper = data_jpsi_crctn_helper if dataset.is_data else mc_jpsi_crctn_helper
-    
-    smearing_helper, smearing_uncertainty_helper = (
-        (None, None) if args.noSmearing else muon_calibration.make_muon_smearing_helpers()
-    )
 
     if dataset.is_data:
         df = df.DefinePerSample("weight", "1.0")
+        df = df.Define("time", brilcalc_helper, ["run", "luminosityBlock"])
+        hist_time = df.HistoBoost("time", [axis_date], ['time'])
     else:
         df = df.Define("weight", "std::copysign(1.0, genWeight)")
     weightsum = df.SumAndCount("weight")
     
+    
+    ### basically do the exact same thing as below but using the GenPart stettings(for generator particle)
+    
+    
+    
     df = df.Define( "isEvenEvent", f"event % 2 {'!=' if args.flipEventNumberSplitting else '=='} 0" ) ## not sure why i have this
-    ## select for two muons, plot their invariant mass
-    
     df = df.Define("muon_pass_pt", "Muon_pt>=25") # [1,0,1,0 ...]
-    df = df.Filter("std::accumulate(muon_pass_pt.begin(), muon_pass_pt.end(), 0) == 2")
+    df = df.Filter("std::accumulate(muon_pass_pt.begin(), muon_pass_pt.end(), 0) == 2") 
 
-    df = df.Define("muon_leading_pt", "Muon_pt[muon_pass_pt][0]") 
-    df = df.Define("muon_subleading_pt", "Muon_pt[muon_pass_pt][1]") 
-    df = df.Define("muon_total_pt", "Muon_pt[muon_pass_pt][0] + Muon_pt[muon_pass_pt][1]")
-    
-    ### attempting to copy this section, really not sure what it means
-                
-    mass_min, mass_max = common.get_default_mz_window()
-    isoBranch = muon_selections.getIsoBranch(args.isolationDefinition)
-    era = args.era
-  
-
-    bias_helper = muon_calibration.make_muon_bias_helpers(args)
-    
-    df = df.Filter(muon_selections.hlt_string(era))
-    df = muon_selections.veto_electrons(df)
-    df = muon_selections.apply_met_filters(df) ## what is a met filter
-    df = muon_calibration.define_corrected_muons(
-        df, cvh_helper, jpsi_helper, args, dataset, smearing_helper, bias_helper
-    )
-
-    df = muon_selections.select_veto_muons(df, nMuons=2)
-    isoThreshold = args.isolationThreshold
-    passIsoBoth = args.muonIsolation[0] + args.muonIsolation[1] == 2
-    df = muon_selections.select_good_muons(
-        df,
-        args.pt[1],
-        args.pt[2],
-        dataset.group,
-        nMuons=2,
-        use_trackerMuons=args.trackerMuons,
-        use_isolation=passIsoBoth,
-        isoBranch=isoBranch,
-        isoThreshold=isoThreshold,
-        requirePixelHits=args.requirePixelHits,
-    )
-
-    df = muon_selections.define_trigger_muons(
-        df, dilepton=args.useDileptonTriggerSelection
-    )
-
-    # iso cut applied here, if requested, because it needs the definition of trigMuons and nonTrigMuons from muon_selections.define_trigger_muons
-    if not passIsoBoth:
-        df = muon_selections.apply_iso_muons(
-            df, args.muonIsolation[0], args.muonIsolation[1], isoBranch, isoThreshold
-        )
-
-    df = df.Define("trigMuons_passIso0", f"{isoBranch}[trigMuons][0] < {isoThreshold}")
     df = df.Define(
-        "nonTrigMuons_passIso0", f"{isoBranch}[nonTrigMuons][0] < {isoThreshold}"
+        "goodTrigObjs",
+        f"wrem::goodMuonTriggerCandidate<wrem::Era::Era_2016PostVFP>(TrigObj_id,TrigObj_filterBits)",
     )
     
-    df = muon_selections.select_z_candidate(df, mass_min, mass_max) ## this is selecting the z to two muon candidates
+    df = df.Define("muon_leading_pt", "Muon_pt[muon_pass_pt][0]") 
+    df = df.Define("muon_leading_eta", "Muon_eta[muon_pass_pt][0]") 
+    df = df.Define("muon_leading_phi", "Muon_phi[muon_pass_pt][0]") 
 
-    #### MUONS HAVE ALL BEEN SELECTED AT THIS POINT ###
+    df = df.Define("muon_subleading_pt", "Muon_pt[muon_pass_pt][1]") 
+    df = df.Define("muon_subleading_eta", "Muon_eta[muon_pass_pt][1]") 
+    df = df.Define("muon_subleading_phi", "Muon_phi[muon_pass_pt][1]")
+    
+    df = df.Define("leading_muon_passTrigger", "wrem::hasTriggerMatch(muon_leading_eta,muon_leading_eta,TrigObj_eta[goodTrigObjs],TrigObj_phi[goodTrigObjs])")
+    df = df.Define("subleading_muon_passTrigger", "wrem::hasTriggerMatch(muon_subleading_eta,muon_subleading_eta,TrigObj_eta[goodTrigObjs],TrigObj_phi[goodTrigObjs])")
+    
+    df = df.Define(
+        "mu_mom4",
+        "ROOT::Math::PtEtaPhiMVector(muon_leading_pt, muon_leading_eta, muon_leading_phi, wrem::muon_mass)")
+    df = df.Define(
+        "smu_mom4",
+        "ROOT::Math::PtEtaPhiMVector(muon_subleading_pt, muon_subleading_eta, muon_subleading_phi, wrem::muon_mass)")
+    df = df.Define(
+        "ll_mom4",
+        f"ROOT::Math::PxPyPzEVector(mu_mom4)+ROOT::Math::PxPyPzEVector(smu_mom4)")
+  
+    df = df.Define("mll", "ll_mom4.mass()")
+    df = df.Filter("mll >= 60 && mll < 120")
+    
+    
+   ### detects two muons but only one has the right momentum
+    dtight = df.Filter("std::accumulate(Muon_tightId.begin(), Muon_tightId.end(), 0) == 2")
 
+    dtight_dtrig = dtight.Filter("subleading_muon_passTrigger && leading_muon_passTrigger" )  
+    dtight_strig = dtight.Filter("subleading_muon_passTrigger != leading_muon_passTrigger" ) 
+    
+    stight_strig = df.Filter("(subleading_muon_passTrigger && Muon_tightId[1]) != (leading_muon_passTrigger && Muon_tightId[0])" ) 
+    
 
     ### SHOULD SEE IF I CAN CHANGE THIS INTO A GLOBAL VARIABLE
     axis_pt = hist.axis.Regular(int(args.pt[0]), args.pt[1], args.pt[2], name="pt")
-    axis_total_pt = hist.axis.Regular(int(args.pt[0]), 50, 118, name="total_pt")
     
     axis_mll = hist.axis.Variable([60,70,75,78,80,82,85,86,87,88,89,90,91,92,93,94,95,96,97,98,100,102,105,110,120], name = 'mll')
     
-
-    hist_total_pt = df.HistoBoost("total_pt", [axis_total_pt], ['muon_total_pt'])
     hist_leading_pt = df.HistoBoost("leading_pt", [axis_pt], ['muon_leading_pt'])
     hist_subleading_pt = df.HistoBoost("subleading_pt", [axis_pt], ['muon_subleading_pt'])
-    hist_mll = df.HistoBoost("mll", [axis_mll], ['mll'])
     
     if dataset.is_data:
-        df = df.Define("time", brilcalc_helper, ["run", "luminosityBlock"])
-
-        hist_time = df.HistoBoost("time", [axis_date], ['time'])
-        hist_time_mll = df.HistoBoost("time_mll", [axis_date, axis_mll], ['time', "mll"])
+        hist_time_mll = dtight_dtrig.HistoBoost("time_mll", [axis_date, axis_mll], ['time', "mll"])
+        hist_time_mll_dtight_strig = dtight_strig.HistoBoost("time_mll_dtight_strig", [axis_date, axis_mll], ['time', "mll"])
+        hist_time_mll_stight_strig = stight_strig.HistoBoost("time_mll_stight_strig", [axis_date, axis_mll], ['time', "mll"])
+        
         results.append(hist_time)
         results.append(hist_time_mll)
-        print("went through loop")
+        results.append(hist_time_mll_dtight_strig)
+        results.append(hist_time_mll_stight_strig)
+    else:
+        hist_mll = dtight_dtrig.HistoBoost("mll", [axis_mll], ['mll', 'weight'])
+        hist_mll_dtight_strig = dtight_strig.HistoBoost("mll_dtight_strig", [axis_mll], ['mll', 'weight'])
+        hist_mll_stight_strig = stight_strig.HistoBoost("mll_stight_strig", [axis_mll], ['mll', 'weight'])
 
-    ### want to make the dimuon object and we need to do that by selecting the muons that also satisfy the trigger criterion. so 
+        results.append(hist_mll)
+        results.append(hist_mll_dtight_strig)
+        results.append(hist_mll_stight_strig)
 
-    results.append(hist_total_pt)
+
     results.append(hist_leading_pt)
     results.append(hist_subleading_pt)
-    results.append(hist_mll)
 
     return results, weightsum
 

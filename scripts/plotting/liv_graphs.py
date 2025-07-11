e8db3e2baea32d1f16d8b84828b325ba1c8feeab
benkjor <jorian.benke@gmail.com>
2025-07-11 12:59:29 -0400

    improved naming conventions

--- scripts/plotting/liv_graphs.py
+++ scripts/plotting/liv_graphs.py
@@ -1,12 +1,19 @@
 import h5py
 import matplotlib.pyplot as plt
+import numpy as np
 
 from utilities.io_tools import input_tools
-from wums.boostHistHelpers import broadcastSystHist, divideHists, multiplyHists
+from wums.boostHistHelpers import (
+    addHists,
+    broadcastSystHist,
+    divideHists,
+    expand_hist_by_duplicate_axis,
+    multiplyHists,
+)
 
 file_in = "/work/submit/jbenke/WRemnants/scripts/histmakers/"
-# file_out = '/home/submit/jbenke/public_html/LIV_mass_evo/'
 file_out = "/home/submit/jbenke/public_html/"
+file_in_name = file_in + "mz_dilepton_liv_scetlib_dyturboCorr.hdf5"
 
 
 def make_plot(data_all, plotname, legend_all=["MC", "Data"], ylim=[]):
@@ -16,108 +23,124 @@ def make_plot(data_all, plotname, legend_all=["MC", "Data"], ylim=[]):
     plt.xlim([0.01, 24])
     if len(ylim) != 0:
         plt.ylim(ylim)
-    # if xaxis != '':
-    #     plt.xlabel(xaxis)
     plt.title(plotname)
     plt.legend(legend_all)
-
     plt.savefig(file_out + plotname + ".png")
 
 
-file_in_name = file_in + "mz_dilepton_liv_scetlib_dyturboCorr.hdf5"
-h5file = h5py.File(file_in_name, "r")
-results = input_tools.load_results_h5py(h5file)
-
-
-time_mll_data = results["dataPostVFP"]["output"]["time_mll"].get()
-mll_data = time_mll_data.project("mll")
-time_data = time_mll_data.project("time")
-stight_strig_data = results["dataPostVFP"]["output"]["time_mll_stight_strig"].get()
-dtight_strig_data = results["dataPostVFP"]["output"]["time_mll_dtight_strig"].get()
-
-mass_mc = results["ZmumuPostVFP"]["output"]["mll"].get()
-stight_strig_mc = results["ZmumuPostVFP"]["output"]["mll_stight_strig"].get()
-dtight_strig_mc = results["ZmumuPostVFP"]["output"]["mll_dtight_strig"].get()
-
-generator = results["ZmumuPostVFP"]["output"]["pass_gen"].get()
-
-lumi_scaling = results["dataPostVFP"]["lumi_outout"]["time"].get()
-weightsum = results["ZmumuPostVFP"]["weight_sum"]
-cross_sec = results["ZmumuPostVFP"]["dataset"]["xsec"]
-
-
-def mc_scaling(mc_results):
+def mc_scaling(mc_results, weightsum, cross_sec):
     mc_results /= weightsum
     mc_results *= cross_sec
     mc_results *= 1000
     return mc_results
 
 
-def all_mc_corrections(hist_in, hist_proj):
-    hist_in = mc_scaling(hist_in)
+def all_mc_corrections(hist_in, hist_proj, weightsum, cross_sec):
+    hist_in = mc_scaling(hist_in, weightsum, cross_sec)
     hist_in_2d = broadcastSystHist(hist_in, hist_proj)
     hist_in_2d = multiplyHists(hist_in_2d, lumi_scaling)
     return hist_in_2d
 
 
-mass_mc_2d = all_mc_corrections(mass_mc, time_mll_data)
-stight_strig_mc_2d = all_mc_corrections(stight_strig_mc, time_mll_data)
-dtight_strig_mc_2d = all_mc_corrections(dtight_strig_mc, time_mll_data)
-generator_2d = all_mc_corrections(generator, time_mll_data)
+h5file = h5py.File(file_in_name, "r")
+results = input_tools.load_results_h5py(h5file)
 
+### accessing the necessary data
+dtdt_data = results["dataPostVFP"]["output"]["time_mll"].get()
+stst_data = results["dataPostVFP"]["output"]["time_mll_stst"].get()
+dtst_data = results["dataPostVFP"]["output"]["time_mll_dtst"].get()
+
+dtdt_mc = results["ZmumuPostVFP"]["output"]["mll_dtdt_prpg"].get().project("mll")
+stst_mc = results["ZmumuPostVFP"]["output"]["mll_stst_prpg"].get().project("mll")
+dtst_mc = results["ZmumuPostVFP"]["output"]["mll_dtst_prpg"].get().project("mll")
+
+generator = results["ZmumuPostVFP"]["output"]["pass_gen"].get()
+
+wsum = results["ZmumuPostVFP"]["weight_sum"]
+xsec = results["ZmumuPostVFP"]["dataset"]["xsec"]
+
+lumi_scaling = results["dataPostVFP"]["lumi_outout"]["lumi_nom"].get()
+lumi_hfoc = results["dataPostVFP"]["lumi_outout"]["lumi_hfoc"].get()
+lumi_pcc = results["dataPostVFP"]["lumi_outout"]["lumi_pcc"].get()
+lumi_ramses = results["dataPostVFP"]["lumi_outout"]["lumi_ramses"].get()
+lumi_hfoc_nom = results["dataPostVFP"]["lumi_outout"]["lumi_in_hfoc"].get()
+lumi_pcc_nom = results["dataPostVFP"]["lumi_outout"]["lumi_in_pcc"].get()
+lumi_ramses_nom = results["dataPostVFP"]["lumi_outout"]["lumi_in_ramses"].get()
+
+
+mll_data = dtdt_data.project("mll")
+time_data = dtdt_data.project("time")
+dtdt_data = expand_hist_by_duplicate_axis(dtdt_data, "mll", "gen_mll")
+
+
+### lumi
+hfoc_scaling = divideHists(lumi_hfoc, lumi_hfoc_nom)
+pcc_scaling = divideHists(lumi_pcc, lumi_pcc_nom)
+ramses_scaling = divideHists(lumi_ramses, lumi_ramses_nom)
+## add the time axis to mc data
+dtdt_mc_2d = all_mc_corrections(dtdt_mc, dtdt_data.project("mll", "time"), wsum, xsec)
+stst_mc_2d = all_mc_corrections(stst_mc, dtdt_data.project("mll", "time"), wsum, xsec)
+dtst_mc_2d = all_mc_corrections(dtst_mc, dtdt_data.project("mll", "time"), wsum, xsec)
+generator_2d = all_mc_corrections(
+    generator, dtdt_data.project("gen_mll", "time"), wsum, xsec
+)
+
+#### should look into whether i need these
+dtdt_data = dtdt_data.project("mll", "time")
+
+
+# pdb.set_trace()
+def empty_hist_copy(hist):
+    # creates am empty hist of the same shape
+    copy_array = np.ones_like(hist.values())
+    copy_hist = hist.copy()
+    copy_hist.values()[...] = copy_array
+    return copy_hist
+
+
+sum_mc = empty_hist_copy(dtdt_mc_2d[{"mll": 0}])
+sum_data = empty_hist_copy(dtdt_data[{"mll": 0}])
+sum_stst_mc = empty_hist_copy(generator_2d[{"gen_mll": 0}])
+sum_dtst_mc = empty_hist_copy(stst_mc_2d[{"mll": 0}])
+sum_generator = empty_hist_copy(dtst_mc_2d[{"mll": 0}])
+
+sum_stst_data = empty_hist_copy(stst_data[{"mll": 0}])
+sum_dtst_data = empty_hist_copy(dtst_data[{"mll": 0}])
 
 for i in range(len(time_data.values())):
+    # for i in range(2):
     ### UGH THIS DOESN'T WORK ANYMORE
-    time_mc_proj = mass_mc_2d[:, i]
-    time_data_proj = time_mll_data[:, i]
-    generator_proj = generator_2d[:, i]
+    time_mc_proj = dtdt_mc_2d[{"mll": i}]
+    time_data_proj = dtdt_data[{"mll": i}]
+    generator_proj = generator_2d[{"gen_mll": i}]
 
-    stight_strig_mc_proj = stight_strig_mc_2d[:, i]
-    dtight_strig_mc_proj = dtight_strig_mc_2d[:, i]
+    stst_mc_proj = stst_mc_2d[{"mll": i}]
+    dtst_mc_proj = dtst_mc_2d[{"mll": i}]
 
-    stight_strig_data_proj = stight_strig_data[:, i]
-    dtight_strig_data_proj = dtight_strig_data[:, i]
+    stst_data_proj = stst_data[{"mll": i}]
+    dtst_data_proj = dtst_data[{"mll": i}]
 
-    if i == 0:
-        mass_sum_mc = time_mc_proj
-        mass_sum_data = time_data_proj
-        mass_sum_stight_strig_mc = stight_strig_mc_proj
-        mass_sum_dtight_strig_mc = dtight_strig_mc_proj
-        mass_sum_generator = generator_proj
-
-        mass_sum_stight_strig_data = stight_strig_data_proj
-        mass_sum_dtight_strig_data = dtight_strig_data_proj
-    else:
-        mass_sum_mc += time_mc_proj
-        mass_sum_data += time_data_proj
-        mass_sum_stight_strig_mc += stight_strig_mc_proj
-        mass_sum_dtight_strig_mc += dtight_strig_mc_proj
-
-        mass_sum_stight_strig_data += stight_strig_data_proj
-        mass_sum_dtight_strig_data += dtight_strig_data_proj
-        mass_sum_generator += generator_proj
-
-    time_mc_proj.plot1d(label="MC: 2 tightID, 2 trig")
-    time_data_proj.plot1d(label="Data: 2 tightID, 2 trig")
-    # stight_strig_mc_proj.plot1d(label = ' MC: 1 tightID, 1 trig')
-    # stight_strig_data_proj.plot1d(label = 'Data: 1 tightID, 1 trig')
-    # dtight_strig_mc_proj.plot1d(label = 'MC: 2 tightID, 1 trig')
-    # dtight_strig_data_proj.plot1d(label = 'Data: 2 tightID, 1 trig')
-    # generator_proj.plot1d("label = Generator")
+    sum_mc = addHists(sum_mc, time_mc_proj)
+    sum_data = addHists(sum_data, time_data_proj)
+    sum_stst_mc = addHists(sum_stst_mc, stst_mc_proj)
+    sum_dtst_mc = addHists(sum_dtst_mc, dtst_mc_proj)
+    sum_stst_data = addHists(sum_stst_data, stst_data_proj)
+    sum_dtst_data = addHists(sum_dtst_data, dtst_data_proj)
+    sum_generator = addHists(sum_generator, generator_proj)
 
     make_plot([time_mc_proj, time_data_proj], "mass_bin_" + str(i))
 
 make_plot(
     [
-        mass_sum_mc,
-        mass_sum_data,
-        mass_sum_stight_strig_mc,
-        mass_sum_stight_strig_data,
-        mass_sum_dtight_strig_mc,
-        mass_sum_dtight_strig_data,
-        mass_sum_generator,
+        sum_mc,
+        sum_data,
+        sum_stst_mc,
+        sum_stst_data,
+        sum_dtst_mc,
+        sum_dtst_data,
+        sum_generator,
     ],
-    "mass_sum_all",
+    "sum_all",
     legend_all=[
         "MC: 2 tightID, 2 trig",
         "Data: 2 tightID, 2 trig",
@@ -129,105 +152,20 @@ make_plot(
     ],
 )
 
-make_plot([mass_sum_generator], "generator", legend_all=["Generator"])
 
-make_plot([mass_sum_mc, mass_sum_data], "mass_sum_new")
-ratio_mass_sum = divideHists(mass_sum_mc, mass_sum_data)
-make_plot([ratio_mass_sum], "mass_sum_ratio", ["MC/Data"], [1, 1.5])
+make_plot(
+    [hfoc_scaling, pcc_scaling, ramses_scaling],
+    "lumi_ratios",
+    ["HFOC/nominal", "PCC/nominal", "RAMSES/nominal"],
+    [0.99, 1.01],
+)
 
+# make_plot([hfoc_scaling], "lumi_ratios", ["HFOC/nominal"], [0.9, 1.1])
+# make_plot([pcc_scaling], "lumi_ratios", ["PCC/nominal"], [0.9, 1.1])
+# make_plot([ramses_scaling], "lumi_ratios", ["RAMSES/nominal"], [0.9, 1.1])
 
-###### this worked the morning of 7/2 ####
-# data_file =
-"""
-time_mll_data = results['dataPostVFP']['output']['time_mll'].get()
-mll_data = time_mll_data.project('mll')
-time_data = time_mll_data.project('time')
+# make_plot([sum_generator], "generator", legend_all=["Generator"])
 
-mass_mc = results['ZmumuPostVFP']['output']['mll'].get() 
-lumi_scaling = results['dataPostVFP']['lumi_outout']['time'].get()
-
-weightsum = results['ZmumuPostVFP']['weight_sum']
-cross_sec = results["ZmumuPostVFP"]["dataset"]["xsec"]
-
-def mc_corrections(mc_results):
-    mc_results /= weightsum
-    mc_results *= cross_sec
-    mc_results *= 1000
-    return mc_results
-
-stight_strig_data = results['dataPostVFP']['output']['time_mll_stight_strig'].get() 
-dtight_strig_data = results['dataPostVFP']['output']['time_mll_dtight_strig'].get() 
-
-stight_strig_mc = results['ZmumuPostVFP']['output']['mll_stight_strig'].get() 
-dtight_strig_mc = results['ZmumuPostVFP']['output']['mll_dtight_strig'].get() 
-
-generator = results['ZmumuPostVFP']['output']['gen_mll'].get()
-
-mass_mc = mc_corrections(mass_mc)
-stight_strig_mc = mc_corrections(stight_strig_mc)
-dtight_strig_mc = mc_corrections(dtight_strig_mc)
-generator = mc_corrections(generator)
-
-mass_mc_2d = broadcastSystHist(mass_mc, time_mll_data)
-stight_strig_mc_2d = broadcastSystHist(stight_strig_mc, time_mll_data)
-dtight_strig_mc_2d = broadcastSystHist(dtight_strig_mc, time_mll_data)
-generator_2d = broadcastSystHist(generator, time_mll_data)
-
-mass_mc_2d = multiplyHists(mass_mc_2d, lumi_scaling)
-stight_strig_mc_2d = multiplyHists(stight_strig_mc_2d, lumi_scaling)
-dtight_strig_mc_2d = multiplyHists(dtight_strig_mc_2d, lumi_scaling)
-generator_2d =  multiplyHists(generator_2d, lumi_scaling)
-
-
-for i in range(len(time_data.values())):
-    ### UGH THIS DOESN'T WORK ANYMORE
-    time_mc_proj = mass_mc_2d[:, i]
-    time_data_proj = time_mll_data[:, i]
-    generator_proj = generator_2d[:, i]
-    
-    stight_strig_mc_proj = stight_strig_mc_2d[:, i]
-    dtight_strig_mc_proj = dtight_strig_mc_2d[:, i]
-    
-    stight_strig_data_proj = stight_strig_data[:, i]
-    dtight_strig_data_proj = dtight_strig_data[:, i]
-    
-    if i == 0: 
-        mass_sum_mc = time_mc_proj
-        mass_sum_data = time_data_proj
-        mass_sum_stight_strig_mc = stight_strig_mc_proj
-        mass_sum_dtight_strig_mc = dtight_strig_mc_proj
-        mass_sum_generator = generator_proj
-
-        mass_sum_stight_strig_data = stight_strig_data_proj
-        mass_sum_dtight_strig_data = dtight_strig_data_proj
-    else:
-        mass_sum_mc += time_mc_proj
-        mass_sum_data += time_data_proj
-        mass_sum_stight_strig_mc += stight_strig_mc_proj
-        mass_sum_dtight_strig_mc += dtight_strig_mc_proj
-
-        mass_sum_stight_strig_data += stight_strig_data_proj
-        mass_sum_dtight_strig_data += dtight_strig_data_proj
-        mass_sum_generator += generator_proj
-
-    
-    time_mc_proj.plot1d(label = 'MC: 2 tightID, 2 trig')
-    time_data_proj.plot1d(label = 'Data: 2 tightID, 2 trig')
-    # stight_strig_mc_proj.plot1d(label = ' MC: 1 tightID, 1 trig')
-    # stight_strig_data_proj.plot1d(label = 'Data: 1 tightID, 1 trig')
-    # dtight_strig_mc_proj.plot1d(label = 'MC: 2 tightID, 1 trig')
-    # dtight_strig_data_proj.plot1d(label = 'Data: 2 tightID, 1 trig')
-    # generator_proj.plot1d("label = Generator")
-    
-    make_plot([time_mc_proj, time_data_proj], "mass_bin_" + str(i))
-
-make_plot([mass_sum_mc, mass_sum_data, mass_sum_stight_strig_mc,  mass_sum_stight_strig_data,mass_sum_dtight_strig_mc, mass_sum_dtight_strig_data, mass_sum_generator], "mass_sum_all", legend_all = ['MC: 2 tightID, 2 trig', 'Data: 2 tightID, 2 trig', 'MC: 1 tightID, 1 trig', 'Data: 1 tightID, 1 trig', 'MC: 2 tightID, 1 trig', 'Data: 2 tightID, 1 trig', 'Generator'])
-
-make_plot([mass_sum_generator], "generator", legend_all = ["Generator"])
-
-make_plot([mass_sum_mc, mass_sum_data], "mass_sum_new")
-ratio_mass_sum = divideHists(mass_sum_mc, mass_sum_data)
-make_plot([ratio_mass_sum], "mass_sum_ratio", ["MC/Data"], [1, 1.5])
-
-
-"""
+make_plot([sum_mc, sum_data], "sum_new")
+ratio_sum = divideHists(sum_mc, sum_data)
+make_plot([ratio_sum], "sum_ratio", ["MC/Data"], [1, 1.5])

6ed9f7ef39df32993054e3b786ff3474664e78c0
benkjor <jorian.benke@gmail.com>
2026-02-20 02:52:09 -0500

    remove unused functions

--- scripts/plotting/uncertainty_tools.py
+++ scripts/plotting/uncertainty_tools.py
@@ -10,73 +10,6 @@ from wums.boostHistHelpers import (
 )
 
 
-def get_h2var(eps_id, eps_hlt, heff):
-    #  h2var = heff * (eps_prime * eps)
-    h2var = multiplyHists(heff, multiplyHists(eps_id, eps_id))
-    h2var = multiplyHists(h2var, multiplyHists(eps_hlt, eps_hlt))
-    return h2var
-
-
-def get_h1var(eps_id, eps_hlt, heff, hist_ones):
-    # h = 2*heff*(eps_hlt * hlt_prime)*(1-(eps_hlt * hlt_prime))
-    h1var = addHists(hist_ones, scaleHist(eps_hlt, -1))
-    h1var = multiplyHists(h1var, eps_hlt)
-    h1var = scaleHist(h1var, 2)
-    h1var = multiplyHists(h1var, multiplyHists(eps_id, eps_id))
-    h1var = multiplyHists(h1var, heff)
-
-    return h1var
-
-
-def get_h1var_low(eps_id, eps_hlt, heff, hist_ones):
-    # h = heff*(eps_hlt * hlt_prime)*(1-(eps_hlt * hlt_prime))
-    # h1var = addHists(hist_ones, scaleHist(eps_hlt, -1))
-    # h1var = multiplyHists(h1var, )
-
-    h1var = multiplyHists(eps_id, eps_id)
-    # h1var = multiplyHists(eps_hlt, multiplyHists(eps_id, eps_id))
-    h1var = multiplyHists(h1var, heff)
-    return h1var
-
-
-def get_h0var(eps_id, eps_hlt, heff, hist_ones):
-    # h = 2*heff*(eps_id * eps_id_prime)*(1-(eps_id * eps_id_prime))
-    h0var = addHists(hist_ones, scaleHist(eps_id, -1))
-    h0var = multiplyHists(h0var, eps_id)
-    h0var = multiplyHists(h0var, eps_hlt)
-    h0var = scaleHist(h0var, 2)
-    h0var = multiplyHists(h0var, heff)
-    return h0var
-
-
-def get_h0var_low(eps_id, eps_hlt, heff, hist_ones):
-    # h = heff*(eps_id * eps_id_prime)*(1-(eps_id * eps_id_prime))
-    h0var = addHists(hist_ones, scaleHist(eps_id, -1))
-    h0var = multiplyHists(h0var, eps_id)
-    # h0var = multiplyHists(h0var, eps_hlt)
-    h0var = multiplyHists(h0var, heff)
-    return h0var
-
-
-def get_eff_hist(eps_hist, ref_hist, i, j, k, first_ind, second_ind):
-    hist_copy = ref_hist.copy()
-    hist_values = hist_copy.values()
-    try:
-        # hist_values[j, i] = eps_hist[{"time": j, f"{first_ind}": i}, f"{second_ind}: k"].value
-
-        hist_values[k, i, j] = eps_hist[
-            {"time": k, f"{first_ind}": i, f"{second_ind}": j}
-        ].value
-    except:
-        # hist_values[j, i] = eps_hist[{"time": j, "mll": i}]
-        hist_values[k, i, j] = eps_hist[
-            {"time": k, f"{first_ind}": i, f"{second_ind}": j}
-        ]
-
-    hist_copy.values()[...] = hist_values
-    return hist_copy
-
-
 def mc_scaling(mc_results, weightsum, cross_sec):
     temp = mc_results.copy()
     temp /= weightsum
@@ -194,7 +127,6 @@ def eta_phi_systematic(
         cross_sec,
     )
 
-    # dtdt_stat = remove_low_bins(dtdt_stat.copy())
     writer.add_systematic(
         iso_stat.project("time", "pt_probe", "eta_probe"),  # , "mll"),
         f"prefiring_stat_etaphi_{etaphi_num}",
@@ -370,9 +302,6 @@ def background_syst(
     dtst_proc = dtst.project("time", "pt_probe", "eta_probe")  # , "mll")
     stst_proc = stst.project("time", "pt_probe", "eta_probe")  # , "mll")
 
-    # iso_proc, dtdt_proc, dtst_proc, stst_proc = make_mutually_exclusive(
-    #     iso_proc, dtdt_proc, dtst_proc, stst_proc
-    # )
     # iso_proc = remove_low_bins(iso_proc)
 
     writer.add_process(iso_proc, f"{proc_name}", "ch_iso", signal=False)
@@ -395,37 +324,6 @@ def background_syst(
     writer.add_norm_systematic(
         f"{bkg_name}", f"{proc_name}", "ch_stst", var, groups=["bkg"]
     )
-
-
-def get_eff_variations(h1_leading, h2_leading, h0_leading, eps_id_prime, eps_hlt_prime):
-    ### no longer used
-    efficiency_ones = make_ones_hist(h1_leading)
-
-    #### okay need to make versions of this for
-    ## e2 = 2*h2/(h1 + 2*h1)
-    eps_hlt = addHists(h1_leading, scaleHist(h2_leading, 2))
-    eps_hlt = divideHists(h2_leading, eps_hlt)
-    eps_hlt = scaleHist(eps_hlt, 2)
-    ##e1 = h1/(h0*(1-e2) + h1)
-    eps_id = addHists(efficiency_ones, scaleHist(eps_hlt, -1))
-    eps_id = multiplyHists(h0_leading, eps_id)
-    eps_id = addHists(eps_id, h1_leading)
-    eps_id = divideHists(h1_leading, eps_id)
-
-    heff = divideHists(h2_leading, multiplyHists(eps_hlt, eps_hlt))
-    heff = divideHists(heff, multiplyHists(eps_id, eps_id))
-
-    # generate histogram of ones
-    eps_id_var = scaleHist(eps_id.copy(), eps_id_prime)
-    eps_hlt_var = scaleHist(eps_hlt.copy(), eps_hlt_prime)
-
-    h0var_id = get_h0var(eps_id_var, eps_hlt, heff, efficiency_ones)
-    h0var_hlt = get_h0var(eps_id, eps_hlt_var, heff, efficiency_ones)
-    h1var_id = get_h1var(eps_id_var, eps_hlt, heff, efficiency_ones)
-    h1var_hlt = get_h1var(eps_id, eps_hlt_var, heff, efficiency_ones)
-    h2var_id = get_h2var(eps_id_var, eps_hlt, heff)
-    h2var_hlt = get_h2var(eps_id, eps_hlt_var, heff)
-    return h0var_id, h0var_hlt, h1var_id, h1var_hlt, h2var_id, h2var_hlt
 
 
 def remove_low_bins(old_hist, ax_name="pt_probe", nbins=1):
@@ -468,14 +366,6 @@ def remove_low_bins(old_hist, ax_name="pt_probe", nbins=1):
         )
         new_hist.values()[...] = old_hist.values()[:, :, nbins:, :, :, :]
 
-    return new_hist
-
-
-def probe_to_tag(old_hist):
-    new_pt_axis = hist.axis.Variable(old_hist.axes[1].edges, name="pt_tag")
-    new_eta_axis = hist.axis.Variable(old_hist.axes[2].edges, name="eta_tag")
-    new_hist = hist.Hist(old_hist.axes[0], new_pt_axis, new_eta_axis)
-    new_hist.values()[...] = old_hist.values()[...]
     return new_hist
 
 
